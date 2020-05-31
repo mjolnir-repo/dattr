117880a435ca9e444b409d7faa228033b58f5b45
Saumalya Sarkar <potter@Saumalyas-MacBook-Air.local>
2020-06-01 00:38:39 +0530

    Data assignment via both interpolation and dot(.) notation is supported(with minor caviates, discussed in the README.md)

--- main.py
+++ main.py
@@ -9,36 +9,23 @@ import sys
 
 class DictAttr(object):
     """
-        Interpretation of dictionary keys as Attributtes using dot(.) notation.
-        The class is designed to be a semi-singleton class to handle recurrsive path update.
+        Interpretation of dictionary keys as Attributtes using dot(.) notation(with minor restrictions).
     """
-    # _DICT_ATTR_OBJ: DictAttr
-    # def __new__(cls, *args, *kwargs):
-    #     """
-    #         If object already available and `singleton` argument is provided as False, return the same.
-    #         Else create new object.
-    #     """
-    #     if cls.__name__ == 'DictAttr':
-    #         if _DICT_ATTR_OBJ and kwargs.get('singleton', False):
-    #             return _DICT_ATTR_OBJ
-    #         else:
-    #             return super(DictAttr, cls).__new__(cls)
-    #     else:
-    #         return super(DictAttr, cls).__new__(cls)
 
     def __init__(self, data:typing.Union[typing.Dict, typing.List, str, int]):
         """
-            Initializing Object.
+            Initializing Object. We will use setattr from object class, as setattr is overriden in DictAttrSub class.
+            param: data
         """
-        self._data = data
-        self._path = []
+        super().__setattr__('_data', data)
+        super().__setattr__('_path', [])
 
-    """Alternate constructors"""
+    # Alternate constructors
     @classmethod
     def from_string(cls, value:str):
         """
             Alternate constructor to build from string representation.
-            param: value:str
+            param: value
         """
         return cls(json.loads(value))
 
@@ -46,7 +33,7 @@ class DictAttr(object):
     def from_json_file(cls, filepath):
         """
             Alternate constructor to build from string representation.
-            param: value:str
+            param: filepath
         """
         if os.path.exists(filepath):
             with open(filepath, 'r') as f:
@@ -56,15 +43,21 @@ class DictAttr(object):
             # TODO: Raise File does not exists exception.
             raise Exception('File does not exists!')
 
-    """Helper function"""
     def _get_current_data(self):
+        """
+            Helper function: Extracts value of current state.
+        """
         data = self._data
         for elem in self._path:
             data = data[elem]
         return data
 
-    """Meta-programming"""
+    # Meta-programming starts
     def __getitem__(self, key):
+        """
+            Handling data extraction via interpolation.
+            param: key
+        """
         if isinstance(key, slice) or isinstance(key, str) or isinstance(key, int):
             current_data = self._get_current_data()
             if isinstance(current_data, abc.Mapping):
@@ -75,14 +68,16 @@ class DictAttr(object):
                 return DictAttrSub(self._data, self._path + [key])
             else:
                 # TODO: Raise Invalid Key execption
-                raise Exception('KeyError')
+                raise Exception('KeyError: ' + str(key))
         else:
             # TODO: Raise Invalid type argument
             raise Exception('Invalid argument type: {}'.format(type(key)))
 
     def __getattr__(self, key:str):
         """
+            Handling data extraction via (.) notation.
             Check if accessed key is an identifier of Python dictionary. Else return the DictAttr object for the corrsponding value of the key.
+            param: key
         """
         current_data = self._get_current_data()
         if hasattr(current_data, key):
@@ -95,31 +90,37 @@ class DictAttr(object):
                 # TODO: Raise Invalid Key execption
                 raise Exception('KeyError')
 
-    """Callable(If required)"""
     def __call__(self, *args, **kwargs):
+        """
+            Make the object Callable(Implement if required)
+        """
         pass
-    
-    """Output handling"""
+
     def __str__(self):
-        # if isinstance(self._get_current_data(), abc.Mapping):
-        #     return f"DictAttr<{[key for key in self._data.keys()]}>"
-        # elif isinstance(self._get_current_data(), abc.MutableSequence):
-        #     elem = "DictAttr<(...)>"
-        #     return f"DictAttr<{[elem for _ in self._data]}>"
-        # else:
-        #     return str(self._get_current_data())
+        """
+            Setting the output of `str()` and `print()` function.
+        """
         return str(self._get_current_data())
-    
+
     def __repr__(self):
-        return 'DictAttr(' + str(self._get_current_data()) + ')'
+        """
+            Setting represtation of the object.
+        """
+        return "DictAttr('" + str(self._get_current_data()) + "')"
 
-    # def to_dict(self) -> typing.Dict:
-    #     return self._data
+    def __eq__(self, other):
+        """
+            Handling Equality, this will come into effect is the object is compared with any data.
+            param: other
+        """
+        return str(self) == str(other)
 
-    def to_string(self) -> str:
-        return json.dumps(self._data)
-    
+    # Alternate data access API
     def to_json_file(self, filepath):
+        """
+            Data will be stored in file in json format.
+            param: filepath
+        """
         if os.path.exists(filepath):
             with open(filepath, 'w') as f:
                 json.dump(self._data, f)
@@ -127,40 +128,60 @@ class DictAttr(object):
             # TODO: Raise File does not exists exception.
             raise Exception('File does not exists!')
 
-    # def __setitem__(self, _index, _value):
-    #     if _index >= len(self.__value):
-    #         # TODO: Raise Index out of bound exception.
-    #         raise Exception("List assignment Index out of bound execption!")
-    #     self.__value[_index] = _value
-
-    # def to_list(self) -> typing.List:
-    #     return self.__value
-
 
 class DictAttrSub(DictAttr):
+    """
+        Keeping the path of each level separate using separate objects.
+    """
+
     def __init__(self, data:typing.Union[typing.Dict, typing.List, str, int], path:list):
+        """
+            Initializing Sub Object. This object will hold the path for current state.
+            param: data
+            param: path
+        """
         super().__init__(data)
-        self._path = path
+        super().__setattr__('_path', path)
+
+    #Meta-programming starts
+    def __setattr__(self, key:str, value):
+        """
+            Supporting data assignment using (.) notation - with minor caviates.
+            param: key
+            param: value
+        """
+        elem = self._get_current_data()
+        elem[key] = value
+
+    def __setitem__(self, key:str, value):
+        """
+            Supporting data assignment via interpolation.
+            param: key
+            param: value
+        """
+        self.__setattr__(key, value)
 
 
+"""
+    Dummy example set, if main.py is executed directly.
+"""
 if __name__ == "__main__":
-    dattr_obj = DictAttr({
+    dattr_obj = dattr.DictAttr({
         "Dummy_Key": "Dummy_Data",
         "Dummy_List": [
             {
                 "Dummy_Mapping": {
                     "keys": "Dummy_Value_In_Mapping"
                 }, 
-                "Dummy_Flag":1
+                "Dummy_Flag":2
             }
         ]})
-    # return DictAttr.from_string('{"Dummy_Key": "Dummy_Data","Dummy_List": [{"Dummy_Mapping": {"keys": "Dummy_Value_In_Mapping"},"Dummy_Flag":100}]}')
-    print(dattr_obj.to_string())
     print(dattr_obj.Dummy_List)
     print(dattr_obj.Dummy_List[0].Dummy_Mapping)
     print(dattr_obj.Dummy_List[0].Dummy_Mapping['keys'])
     print(dattr_obj.Dummy_List[0].Dummy_Mapping.keys())
-    # dattr_obj.Dummy_List[0] = 'Dummy_Replaced_Value'
-    # print(dattr_obj.Dummy_List[0])
-    # print(dattr_obj.to_string())
-    # print(dattr_obj.Dummy_Mapping.keys())
+    dattr_obj.Dummy_List[0].Dummy_Mapping['keys'] = 'Dummy_Replaced_Value'
+    print(dattr_obj.Dummy_List[0].Dummy_Mapping['keys'])
+    print(dattr_obj.Dummy_List[0].Dummy_Mapping['keys'] == 'Dummy_Replaced_Value')
+    for elem in dattr_obj.keys():
+        print(dattr_obj[elem])
