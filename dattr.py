520cfb1d843ce72e57e4b1748f75c90c0b9396f1
Saumalya Sarkar <potter@Saumalyas-MacBook-Air.local>
2020-05-31 22:24:26 +0530

    Trimmed Down the process of getattr\!

--- dattr.py
+++ dattr.py
@@ -7,193 +7,159 @@ import sys
 
 # from dattr_helper import DictAttrDict
 
-
 class DictAttr(object):
     """
         Interpretation of dictionary keys as Attributtes using dot(.) notation.
+        The class is designed to be a semi-singleton class to handle recurrsive path update.
     """
-    def __new__(cls, _obj:typing.Union[typing.Dict, typing.List, str]):
-        """
-            1. If received object is Mapping, return new object of the class,
-            2. If it is a list, return list of objects,
-            3. Else, return object.
-        """
-        if cls.__name__ == 'DictAttr':
-            if isinstance(_obj, abc.Mapping):
-                return DictAttrDict(_obj)
-            elif isinstance(_obj, abc.MutableSequence):
-                return DictAttrList(_obj)
-            else:
-                return _obj
-        else:
-            return super(DictAttr, cls).__new__(cls)
+    # _DICT_ATTR_OBJ: DictAttr
+    # def __new__(cls, *args, *kwargs):
+    #     """
+    #         If object already available and `singleton` argument is provided as False, return the same.
+    #         Else create new object.
+    #     """
+    #     if cls.__name__ == 'DictAttr':
+    #         if _DICT_ATTR_OBJ and kwargs.get('singleton', False):
+    #             return _DICT_ATTR_OBJ
+    #         else:
+    #             return super(DictAttr, cls).__new__(cls)
+    #     else:
+    #         return super(DictAttr, cls).__new__(cls)
 
+    def __init__(self, data:typing.Union[typing.Dict, typing.List, str, int]):
+        """
+            Initializing Object.
+        """
+        self._data = data
+        self._path = []
+
+    """Alternate constructors"""
     @classmethod
-    def from_string(cls, _value:str):
+    def from_string(cls, value:str):
         """
             Alternate constructor to build from string representation.
             param: value:str
         """
-        return cls(json.loads(_value))
+        return cls(json.loads(value))
 
     @classmethod
-    def from_json_file(cls, _filepath):
+    def from_json_file(cls, filepath):
         """
             Alternate constructor to build from string representation.
             param: value:str
         """
-        if os.path.exists(_filepath):
-            with open(_filepath, 'r') as _f:
-                __json_data = json.load(_f)
-            return cls(__json_data)
+        if os.path.exists(filepath):
+            with open(filepath, 'r') as f:
+                json_data = json.load(f)
+            return cls(json_data)
         else:
             # TODO: Raise File does not exists exception.
             raise Exception('File does not exists!')
 
+    """Helper function"""
+    def _get_current_data(self):
+        data = self._data
+        for elem in self._path:
+            data = data[elem]
+        return data
 
-class DictAttrDict(DictAttr):
-    """
-        When the object is mapping
-    """
-    def __init__(self, _dict_obj:typing.Dict):
-        """
-            Initializing Object.
-        """
-        self.__value = {}
-        for _key, _value in _dict_obj.items():
-            __key = self.__key_check(_key)
-            self.__value[__key] = _value
-
-    def __key_check(self, _key:str):
-        """
-            Checks:
-                1. If provided key is a built-in dictionary attribute, raise exception.
-                2. If provided key is a keyword, append '_' to the end.
-                3. if provided key is not suited to be an identifier, raise exception
-        """
-        if _key in dir(dict()):
-            # TODO: Raise Unsupported Identifier execption
-            raise Exception('Unsupported Identifier execption!')
-        elif keyword.iskeyword(_key) or _key in dir(self):
-            return _key + '_'
-        elif not _key.isidentifier():
-            # TODO: Raise Invalid Identifier execption
-            raise Exception('Invalid Identifier execption!')
+    """Meta-programming"""
+    def __getitem__(self, key):
+        if isinstance(key, slice) or isinstance(key, str) or isinstance(key, int):
+            current_data = self._get_current_data()
+            if isinstance(current_data, abc.Mapping):
+                val = current_data.get(key, None)
+            else:
+                val = current_data[key]
+            if val:
+                return DictAttrSub(self._data, self._path + [key])
+            else:
+                # TODO: Raise Invalid Key execption
+                raise Exception('KeyError')
         else:
-            return _key
+            # TODO: Raise Invalid type argument
+            raise Exception('Invalid argument type: {}'.format(type(key)))
 
-    def __getattr__(self, _key:str):
+    def __getattr__(self, key:str):
         """
             Check if accessed key is an identifier of Python dictionary. Else return the DictAttr object for the corrsponding value of the key.
         """
-        if hasattr(self.__value, _key):
-            return getattr(self.__value, _key)
+        current_data = self._get_current_data()
+        if hasattr(current_data, key):
+            return getattr(current_data, key)
         else:
-            __key = self.__key_check(_key)
-            __val = self.__value.get(__key, None)
-            if __val:
-                return DictAttr(__val)
+            val = current_data.get(key, None)
+            if val:
+                return DictAttrSub(self._data, self._path + [key])
             else:
                 # TODO: Raise Invalid Key execption
                 raise Exception('KeyError')
 
-    # def __setitem__(self, _key, _value):
-    #     __key = self.__key_check(_key)
-    #     self.__value[__key] = _value
-
+    """Callable(If required)"""
     def __call__(self, *args, **kwargs):
         pass
     
+    """Output handling"""
     def __str__(self):
-        return f"DictAttr<{[_key for _key in self.__value.keys()]}>"
+        # if isinstance(self._get_current_data(), abc.Mapping):
+        #     return f"DictAttr<{[key for key in self._data.keys()]}>"
+        # elif isinstance(self._get_current_data(), abc.MutableSequence):
+        #     elem = "DictAttr<(...)>"
+        #     return f"DictAttr<{[elem for _ in self._data]}>"
+        # else:
+        #     return str(self._get_current_data())
+        return str(self._get_current_data())
     
     def __repr__(self):
-        return f"DictAttr<{[_key for _key in self.__value.keys()]}>"
+        return 'DictAttr(' + str(self._get_current_data()) + ')'
 
-    def to_dict(self) -> typing.Dict:
-        return self.__value
+    # def to_dict(self) -> typing.Dict:
+    #     return self._data
 
     def to_string(self) -> str:
-        return json.dumps(self.__value)
+        return json.dumps(self._data)
     
-    def to_json_file(self, _filepath):
-        if os.path.exists(_filepath):
-            with open(_filepath, 'w') as _f:
-                json.dump(self.__value, _f)
+    def to_json_file(self, filepath):
+        if os.path.exists(filepath):
+            with open(filepath, 'w') as f:
+                json.dump(self._data, f)
         else:
             # TODO: Raise File does not exists exception.
             raise Exception('File does not exists!')
 
-class DictAttrList(DictAttr):
-    """
-        When the object is MutableSequence/List
-    """
-    def __init__(self, _list_obj:typing.List):
-        """
-            Initializing Object.
-        """
-        self.__value = _list_obj
+    # def __setitem__(self, _index, _value):
+    #     if _index >= len(self.__value):
+    #         # TODO: Raise Index out of bound exception.
+    #         raise Exception("List assignment Index out of bound execption!")
+    #     self.__value[_index] = _value
 
-    def __setitem__(self, _index, _value):
-        if _index >= len(self.__value):
-            # TODO: Raise Index out of bound exception.
-            raise Exception("List assignment Index out of bound execption!")
-        self.__value[_index] = _value
+    # def to_list(self) -> typing.List:
+    #     return self.__value
 
-    def _get_value(self, _index):
-        if _index >= len(self.__value):
-            # TODO: Raise Index out of bound exception.
-            raise Exception("Index out of bound execption!")
-        return DictAttr(self.__value[_index])
 
-    def __getitem__(self, _key):
-        if isinstance(_key, slice):
-            _start, _stop, _step = _key.indices(len(self))
-            return DictAttr([self[i] for i in range(_start, _stop, _step)])
-        elif isinstance(_key, int):
-            return self._get_value(_key)
-        else:
-            # TODO: Raise Invalid type argument
-            raise Exception('Invalid argument type: {}'.format(type(_key)))
+class DictAttrSub(DictAttr):
+    def __init__(self, data:typing.Union[typing.Dict, typing.List, str, int], path:list):
+        super().__init__(data)
+        self._path = path
 
-    def __str__(self):
-        _elem = "DictAttr<(...)>"
-        return f"DictAttr<{[_elem for _ in self.__value]}>"
-    
-    def __repr__(self):
-        _elem = "DictAttr<(...)>"
-        return f"DictAttr<{[_elem for _ in self.__value]}>"
 
-    def to_list(self) -> typing.List:
-        return self.__value
-
-    def to_string(self) -> str:
-        return json.dumps(self.__value)
-    
-    def to_json_file(self, _filepath):
-        if os.path.exists(_filepath):
-            with open(_filepath, 'w') as _f:
-                json.dump(self.__value, _f)
-        else:
-            # TODO: Raise File does not exists exception.
-            raise Exception('File does not exists!')
-
-# if __name__ == "__main__":
-    # dattr_obj = DictAttr({
-    #     "Dummy_Key": "Dummy_Data",
-    #     "Dummy_List": [
-    #         {
-    #             "Dummy_Mapping": {
-    #                 "keys_": "Dummy_Value_In_Mapping"
-    #             }, 
-    #             "Dummy_Flag":1
-    #         }
-    #     ]})
-    # return DictAttr.from_string('{"Dummy_Key": "Dummy_Data","Dummy_List": [{"Dummy_Mapping": {"keys_": "Dummy_Value_In_Mapping"},"Dummy_Flag":100}]}')
-    # print(dattr_obj.to_string())
-    # print(dattr_obj.Dummy_List)
-    # print(dattr_obj.Dummy_List[0].Dummy_Mapping.keys_)
-    # print(dattr_obj.Dummy_List.to_list())
+if __name__ == "__main__":
+    dattr_obj = DictAttr({
+        "Dummy_Key": "Dummy_Data",
+        "Dummy_List": [
+            {
+                "Dummy_Mapping": {
+                    "keys": "Dummy_Value_In_Mapping"
+                }, 
+                "Dummy_Flag":1
+            }
+        ]})
+    # return DictAttr.from_string('{"Dummy_Key": "Dummy_Data","Dummy_List": [{"Dummy_Mapping": {"keys": "Dummy_Value_In_Mapping"},"Dummy_Flag":100}]}')
+    print(dattr_obj.to_string())
+    print(dattr_obj.Dummy_List)
+    print(dattr_obj.Dummy_List[0].Dummy_Mapping)
+    print(dattr_obj.Dummy_List[0].Dummy_Mapping['keys'])
+    print(dattr_obj.Dummy_List[0].Dummy_Mapping.keys())
     # dattr_obj.Dummy_List[0] = 'Dummy_Replaced_Value'
     # print(dattr_obj.Dummy_List[0])
     # print(dattr_obj.to_string())
