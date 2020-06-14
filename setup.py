1fc0c81f6a15a0f18332301b8971a88abdbf7ea7
Saumalya Sarkar <potter@Saumalyas-MacBook-Air.local>
2020-06-14 20:20:26 +0530

    PyPI package is created .

--- setup.py
+++ setup.py
@@ -1,26 +1,32 @@
 from distutils.core import setup
+
+with open("README", "r") as fh:
+    long_description = fh.read()
+
 setup(
-  name = 'dattr',
-  packages = ['dattr'],
-  version = '1.0',
-  license='MIT',
-  description = 'This package was developed as support package to mjonir project. In mjolnir project, there are certain scope to read configuration json and traverse through then recursively. So I felt the need for a solution that will let us chain the dictionary keys with dot notation for ease-of-use. Hence I built one. I know there are already some libraries available which do similar stuff. But the purpose of this package was to support custom features like reading-writing to-fro json files, read strings and compile as dictionary directly etc.',   # Give a short description about your library
-  author = 'Saumalya Sarkar',
-  author_email = 'saumalya75@gmail.com',
-  url = 'https://github.com/mjolnir-repo/dattr',
-  download_url = 'https://github.com/mjolnir-repo/dattr/archive/v1.0.tar.gz',
-  keywords = ['python'],
-  install_requires=[],
-  classifiers=[
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development',
-    'License :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-  ],
+    name = 'dattr',
+    packages = ['dattr'],
+    version = '1.5.0',
+    license='MIT',
+    description = 'Use the package to use python dictionary keys as attributes using dot(.) notation.',
+    author = 'Saumalya Sarkar',
+    author_email = 'saumalya75@gmail.com',
+    url = 'https://github.com/mjolnir-repo/dattr',
+    download_url = 'https://github.com/mjolnir-repo/dattr/archive/v1.5.0.tar.gz',
+    keywords = ['python'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+    long_description = long_description,
+    long_description_content_type = "text/markdown"
 )
